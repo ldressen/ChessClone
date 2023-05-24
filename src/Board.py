a1da79155616488ee89aecc5b0bb0e1e6b0c393f
Luke <luke.dressen@alumni.fh-aachen.de>
2023-05-24 21:52:31 +0200

    implemented pawn moves + basic undo/forward moves

--- src/Board.py
+++ src/Board.py
@@ -3,6 +3,7 @@ import os
 import numpy as np
 import pygame as p
 
+
 class Board:
     DIMENSION = 8
     square_size = 0
@@ -11,6 +12,10 @@ class Board:
         self.screen = screen
         self.square_size = size / self.DIMENSION
         self.moveLog = []
+        self.current_move = 0
+        self.undo_idx = 0
+        self.is_log = False
+        self.move_log_index = 0
         self.whites_turn = True
         self.images = {}
         self.board = np.array([
@@ -28,23 +33,163 @@ class Board:
         self.draw_pieces()
 
     def load_images(self):
+        """
+        Loads the images for the pieces and stores it in dictionairy.
+        """
         path = "../res/pieces/"
         for piece in sorted(os.listdir(path)):
             self.images[piece.replace(".png", "")] = p.image.load(path + piece)
 
     def draw_board(self):
+        """
+        Draws the basic 8x8 chessboard layout.
+        """
         colors = [p.Color("White"), p.Color("gray")]
         for row in range(self.DIMENSION):
             for col in range(self.DIMENSION):
-                color = colors[(row+col) % 2]
-                p.draw.rect(self.screen, color, p.Rect(col * self.square_size, row * self.square_size, self.square_size, self.square_size))
+                color = colors[(row + col) % 2]
+                p.draw.rect(self.screen, color,
+                            p.Rect(col * self.square_size, row * self.square_size, self.square_size, self.square_size))
 
     def draw_pieces(self):
+        """
+        Draws the pieces on their current position.
+        """
         for row in range(self.DIMENSION):
             for col in range(self.DIMENSION):
                 piece = self.board[row, col]
                 if piece != "-":
-                    self.screen.blit(self.images[piece], p.Rect(col * self.square_size, row * self.square_size, self.square_size, self.square_size))
+                    self.screen.blit(self.images[piece],
+                                     p.Rect(col * self.square_size, row * self.square_size, self.square_size,
+                                            self.square_size))
 
+    def get_tile_from_pixel_coords(self, pos: tuple):
+        """
+        Convert pixel coordinates into corresponding tile that was clicked on.
+        :param pos: tuple
+        :return: x_tile, ytile: tuple
+        """
+        x_tile: int = int(pos[0] // self.square_size)
+        y_tile: int = int(pos[1] // self.square_size)
 
+        return x_tile, y_tile
 
+    def clicked_on_tile(self, tile: tuple):
+        """
+        Highlights the tile the player clicked on when a piece is there.
+        :param tile: tuple
+        """
+        p.draw.rect(self.screen, p.Color(211, 211, 211),
+                    p.Rect(tile[0] * self.square_size, tile[1] * self.square_size, self.square_size,
+                           self.square_size))
+        self.draw_pieces()
+
+    def update_board(self):
+        """
+        Updates the board and the pieces.
+        """
+        self.draw_board()
+        self.draw_pieces()
+
+    def move_piece(self, tile1, tile2):
+        """
+        Moves piece from one tile to another.
+        :param tile1: tuple
+        :param tile2: tuple
+        """
+        piece = self.board[tile1[1], tile1[0]]
+        self.board[tile2[1], tile2[0]] = piece
+        self.board[tile1[1], tile1[0]] = "-"
+        self.update_board()
+        self.moveLog.append((tile1, tile2))
+        self.current_move += 1
+        self.undo_idx += 1
+        print(self.current_move)
+        print(self.moveLog)
+
+    def move_piece_index_back(self, idx):
+        """
+        Makes the move of a piece backwards.
+        :param idx: int
+        """
+        move = self.moveLog[idx]
+        tile1 = move[0]
+        tile2 = move[1]
+        piece = self.board[tile2[1], tile2[0]]
+        self.board[tile1[1], tile1[0]] = piece
+        self.board[tile2[1], tile2[0]] = "-"
+        self.undo_idx -= 1
+        print(self.undo_idx)
+        self.update_board()
+
+    def move_piece_index_forward(self, idx):
+        """
+        Makes the move of a piece forward.
+        :param idx: int
+        """
+        move = self.moveLog[idx - 1]
+        tile1 = move[0]
+        tile2 = move[1]
+        piece = self.board[tile1[1], tile1[0]]
+        self.board[tile2[1], tile2[0]] = piece
+        self.board[tile1[1], tile1[0]] = "-"
+        self.undo_idx += 1
+        print(self.undo_idx)
+        self.update_board()
+
+    def can_move(self):
+        if self.current_move == self.undo_idx:
+            return True
+        return False
+
+    def undo_move(self):
+        """
+        Undos move and displays the move before.
+        """
+        if len(self.moveLog) > 0 and self.undo_idx > 0:
+            self.move_piece_index_back(self.undo_idx - 1)
+
+    def move_forward(self):
+        """
+        Displays next move that was played.
+        """
+        if len(self.moveLog) > 0 and self.undo_idx < self.current_move:
+            self.move_piece_index_forward(self.undo_idx + 1)
+
+    def get_all_possible_moves(self, tile):
+        """
+        Returns all move a particular piece can perform on a certain tile.
+        :param tile: tuple
+        :return: list(tuple)
+        """
+        if self.board[tile[1], tile[0]] == "-":
+            return 0
+
+        # white pawns
+        if self.board[tile[1], tile[0]] == "wP" and self.board[tile[1] - 1, tile[0]] == "-":
+            if tile[1] == 6 and self.board[tile[1] - 2, tile[0]] == "-":
+                self.draw_move_preview((tile[0], tile[1] - 1))
+                self.draw_move_preview((tile[0], tile[1] - 2))
+                return [(tile[0], tile[1] - 1), (tile[0], tile[1] - 2)]
+            else:
+                self.draw_move_preview((tile[0], tile[1] - 1))
+                return [(tile[0], tile[1] - 1)]
+
+        # black pawns
+        if self.board[tile[1], tile[0]] == "bP" and self.board[tile[1] + 1, tile[0]] == "-":
+            if tile[1] == 1 and self.board[tile[1] + 2, tile[0]] == "-":
+                self.draw_move_preview((tile[0], tile[1] + 1))
+                self.draw_move_preview((tile[0], tile[1] + 2))
+                return [(tile[0], tile[1] + 1), (tile[0], tile[1] + 2)]
+            else:
+                self.draw_move_preview((tile[0], tile[1] + 1))
+                return [(tile[0], tile[1] + 1)]
+
+    def draw_move_preview(self, tile):
+        """
+        Draws the circles to indicate possible moves.
+        :param tile: tuple
+        """
+        p.draw.circle(self.screen, p.Color((211, 211, 211)), (
+        tile[0] * self.square_size + self.square_size / 2, tile[1] * self.square_size + self.square_size / 2),
+                      self.square_size / 3)
--- src/Game.py
+++ src/Game.py
@@ -2,7 +2,7 @@ import pygame as p
 from Board import Board
 
 WIDTH, HEIGHT = 512, 512
-MAX_FPS = 15
+MAX_FPS = 60
 class Game:
 
     run = True
@@ -13,11 +13,37 @@ class Game:
         self.clock = p.time.Clock()
 
     def main_loop(self):
-
+        on_tile_clicked = False
+        store_tile = None
+        possible_moves = None
         while self.run:
             for event in p.event.get():
                 if event.type == p.QUIT:
                     self.run = False
+                if event.type == p.MOUSEBUTTONUP:
+
+                    pos = p.mouse.get_pos()
+                    tile = self.board.get_tile_from_pixel_coords(pos)
+                    if self.board.board[tile[1], tile[0]] != "-" and not on_tile_clicked and self.board.can_move():
+                        on_tile_clicked = True
+                        store_tile = tile
+                        self.board.draw_board()
+                        self.board.clicked_on_tile(tile)
+                        possible_moves = self.board.get_all_possible_moves(tile)
+                        if possible_moves is None:
+                            on_tile_clicked = False
+                    # only really move piece if tile is empty (TODO: or can take) and there are possible moves and
+                    #  you are not on an  earlier move
+                    elif self.board.board[tile[1], tile[0]] == "-" and on_tile_clicked and possible_moves is not None and self.board.can_move():
+                        if tile in possible_moves:
+                            self.board.move_piece(store_tile, tile)
+                        on_tile_clicked = False
+
+                if event.type == p.KEYDOWN:
+                    if event.key == p.K_LEFT:
+                        self.board.undo_move()
+                    elif event.key == p.K_RIGHT:
+                        self.board.move_forward()
 
             self.clock.tick(MAX_FPS)
             p.display.flip()
